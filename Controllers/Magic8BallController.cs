b77d94902f44c374c5f2b7478beb06b6dec19dca
jacob <jacob.dekok01@gmail.com>
2022-10-27 14:52:41 -0700

    Peer Review

--- Controllers/Magic8BallController.cs
+++ Controllers/Magic8BallController.cs
@@ -2,7 +2,7 @@
 // 10-27-22
 // Magic 8 Ball - Endpoint
 // Create a Magic 8 Ball WebAPI project that takes in a string variable via the URL as a question and presents the user with a randomly generated answer
-// Peer reviewed by
+// Peer reviewed by: Jacob Dekok - The program runs perfectly and I found no errors. I like how he added two routes the program could take, one if you don't want to type out the question and the other will output your question along with an answer from the magic 8. The code is very organized and I see he used some of the things we learned in todays lecture(10/27/2022) like putting the route with the [HttpGet("...")]
 
 using System;
 using System.Collections.Generic;
